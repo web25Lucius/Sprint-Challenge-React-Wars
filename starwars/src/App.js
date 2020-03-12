d54985b3d9e46f5252a7fff74375775c90d2e3aa
Mona B <web25Lucius@gmail.com>
2020-03-12 00:44:45 -0600

    fixed fetch, building component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,38 +1,43 @@
 import React , {useState, useEffect} from 'react';
 import './App.css';
 
-
-function Characters () {
-const [data, setData] = useState (); 
-const [Error, setError] = useState (); 
-
-  useEffect (() => {
-    fetch('http https://swapi.co/api/people/names/')
-    // console.log(Response);
-    .then (Response => setData(Response.data))
-    .catch(Error => setError(Error.Response))
-  }, []);  
-
-}
-
 const App = () => {
- 
-
-
+  const [characters, setCharacters] = useState(); 
+  const fetchData = () =>
+      fetch('https://swapi.co/api/people/')
+      .then(Response => Response.json())
+      .then(characters =>{
+        const chArray = setCharacters(characters.results); 
+      })
+      .catch(Response => "ERROR! ERROORRRRR!", Error); 
+    
+     
+   
+    
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
+    
     <div className="App">
       <h1 className="Header">React Wars</h1>
       <div>setData</div>
-    </div>
+      <div>
+        <span>{JSON.stringify.setCharacters}</span>
+        {useEffect(() => {
+      fetchData()
+    })}
     
-  );
+      </div>
+    </div>
+
+  
+    
+  )
 }
+
 
 export default App;
